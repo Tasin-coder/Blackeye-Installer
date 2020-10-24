174264e4db883cae635b4aabdc55f43a8997ca32
Tasin-coder <61790940+Tasin-coder@users.noreply.github.com>
2020-10-24 18:06:13 +0600

    Update

--- Blackeye-Installer.sh
+++ Blackeye-Installer.sh
@@ -1,7 +1,5 @@
-cd 
+cd
 clear
-
-pkg install git
 
 echo -e '\033[92m'
 echo ' ___           _        _ _ _              '
@@ -10,13 +8,20 @@ echo ' | ||  _ \/ __| __/ _  | | | |  _ \ / _  | '
 echo ' | || | | \__ \ || (_| | | | | | | | (_| | '
 echo '|___|_| |_|___/\__\__,_|_|_|_|_| |_|\__, | '
 echo '                                    |___/  '
-
-echo -e '\033[93mCloning to Blackeye'
-git clone https://github.com/An0nUD4Y/blackeye
-
-echo -e '\033[93mInstalling Requirements'
-pkg install php curl wget unzip
-
+echo '\033[93mInstalling Git\033[00m'
+pkg install git
+echo "\033[93mSetting up Ngrok\033[00m"
 cd
-cd blackeye 
+git clone https://github.com/Tasin-coder/tunnel
+cd tunnel/intlr.sh
+bash installer.sh
+cd
+echo -e '\033[93mCloning to Blackeye\033[00m'
+git clone https://github.com/Tasin-coder/blackeye
+
+echo -e '\033[93mInstalling Requirements\033[00m'
+pkg install php curl wget unzip -y
+rm -rf tunnel
+cd
+cd blackeye
 bash blackeye.sh
